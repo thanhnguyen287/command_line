3d3423e4d07e1dcb0ea9ee11b68540b92db4289f
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-18 09:19:34 +0100

    Update folder.js

--- serve/helpers/folder.js
+++ serve/helpers/folder.js
@@ -15,7 +15,7 @@ const makeDir = function myFunction(directory,username){
   }
   try {
     fs.mkdirSync('./user_data/' + username + "/" +  directory);
-    fs.appendFile('./user_data/' + username + "/" +  directory+ "/"+'fichier.txt','oumaima',(err)=> {if(err) {}});
+    fs.appendFile('./user_data/' + username + "/" +  directory+ "/"+'fichier.txt','oumaima',(err)=> {if(err) {return (e)}});
     return true;
   } 
   catch (e) {
