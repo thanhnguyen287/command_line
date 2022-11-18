844fa99e48122e5b45798f06c307a40f80a98116
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-18 21:54:50 +0100

    Update routes.js

--- serve/routes.js
+++ serve/routes.js
@@ -64,7 +64,7 @@ module.exports = function(app, passport){
 	
 	app.get('/cat', function(req, res) {
 		var path = "./user_data" +  "/"+ req.user.local.email + "/" + req.query.directory+ "/" + "fichier.txt";
-		fs.readFileSync(path,'utf8',function(err,items)
+		fs.readFile(path,'utf8',function(err,items)
 		{
 			//return list of files contained in a folder.
 		return  res.send( { value : items });
