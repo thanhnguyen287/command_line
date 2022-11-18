fa4f0137e987b18212353460837adc5705eb3fe6
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-18 01:40:00 +0100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -654,6 +654,103 @@ $('textarea').keyup(function(e) {
       }
     }
 
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  else if(command=="cat"){
+
+      if(!logged)
+      {
+        $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~$ </span><span>' + command + '</span></div></div>');
+        $('.terminal-output').append('<div class="result"><div style="width: 100%;"><span>You need to login first</span></div></div><br>');
+        reset();
+      }
+      else 
+      {        
+        $.ajax({
+        type:'get',
+        datatype :'json',
+        data:{ username : username , directory : directory},   
+        url:"/cat"
+        }).done(function(data){
+            if(directory=="")
+              $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~' + directory+ '$ </span><span>' + command + '</span></div></div>'); 
+            else  
+              $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~/' + directory+ '$ </span><span>' + command + '</span></div></div>'); 
+
+            for(num = 0;num < data.value.length ;num++)
+            {
+                $('.terminal-output').append('<div class="folder"><div style="width: 100%;"><span> ' + data.value[num] + '</span></div></div>');
+
+            }              
+            $('.terminal-output').append('<div class="result"><div style="width: 100%;"><span><br></span></div></div>');            
+            reset();
+
+        }).fail(function(jqXHR,exception){
+            $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~$ </span><span>' + command + '</span></div></div>');
+                
+            var msg = '';
+            if (jqXHR.status === 0) {
+              msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+              msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+              msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+              msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') { 
+              msg = 'Time out error.';
+            } else if (exception === 'abort') {
+              msg = 'Ajax request aborted.';
+            } else {
+              msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            if (data) console.error(data)
+            else console.log('Success!')
+          }
+          $('.terminal-output').append('<div class="result"><div style="width: 100%;"><span>ls: cannot list files "'+ command.slice(2) + '"  : ' + msg + '</span></div></div><br>');
+          reset();
+      });
+      }
+    }
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
     else if(command=="logout"){
         if(logged)
         {
