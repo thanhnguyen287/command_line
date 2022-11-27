f7c525eff85b6a63ee3b0725325df9c0fd78cccc
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-27 12:10:04 +0100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -585,7 +585,7 @@ $('textarea').keyup(function(e) {
       if(!logged)
       {
 	   
-        $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~$ </span><span>' + command + ' fichier.txt</span></div></div>');
+        $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~$ </span><span>' + command + '</span></div></div>');
         $('.terminal-output').append('<div class="result"><div style="width: 100%;"><span>You need to login first</span></div></div><br>');
         reset();
       }
@@ -599,9 +599,9 @@ $('textarea').keyup(function(e) {
         url:"/cat"
         }).done(function(data){
             if(directory=="")
-              $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~' + directory+ '$ </span><span>' + command +' fichier.txt</span></div></div>'); 
+              $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~' + directory+ '$ </span><span>' + command +'</span></div></div>'); 
             else  
-              $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~/' + directory+ '$ </span><span>' + command + ' fichier.txt</span></div></div>'); 
+              $('.terminal-output').append('<div class="command" role="presentation" aria-hidden="true"><div style="width: 100%;"><span class="user">root@' + username + ': ~/' + directory+ '$ </span><span>' + command + '</span></div></div>'); 
 
             for(num = 0;num < data.value.length ;num++)
             {
