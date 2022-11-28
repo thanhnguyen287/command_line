00ad0fc08866ce1b721425d47bd80fff46691f50
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-28 14:46:33 +0100

    Update routes.js

--- serve/routes.js
+++ serve/routes.js
@@ -20,8 +20,8 @@ module.exports = function (app, passport) {
     }
   });
 
-  app.get("/views/image1", function (req, res) {
-    res.render("image1", {
+  app.get("/image1", function (req, res) {
+    res.render("/views/image1", {
       user: req.user,
     });
   });
